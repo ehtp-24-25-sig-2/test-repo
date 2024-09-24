e4969584ae60d085a643e8baa4e9136be6844bb2
lechgarh <lechgar.hatim@ehtp.ac.ma>
2024-09-24 11:35:42 +0200

    generate NPE

--- src/main/java/Hello_World.java
+++ src/main/java/Hello_World.java
@@ -1,7 +1,16 @@
 public class Hello_World {
     //comment test
     public static void main(String[] args) {
+        String text = getText();  // This method might return null
+
+        // Risk: Dereferencing the variable 'text' without checking if it's null
+        System.out.println(text.length());
         System.out.println("Hello, World!");
+    }
+
+    private static String getText() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getText'");
     }
 
     public void methodC() {
