d51e19cc3cd6d0a3ea1cbf61d4741d99edb583d0
lechgarh <lechgar.hatim@ehtp.ac.ma>
2024-09-24 11:40:26 +0200

    test

--- src/main/java/Hello_World.java
+++ src/main/java/Hello_World.java
@@ -5,18 +5,13 @@ public class Hello_World {
 
         // Risk: Dereferencing the variable 'text' without checking if it's null
         System.out.println(text.length());
-        System.out.println("Hello, World!");
     }
 
     private static String getText() {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getText'");
     }
 
     public void methodC() {
-        System.out.println("This is method A");
-        System.out.println("This is method A");
-        System.out.println("This is method A");
         System.out.println("This is method A");
     }
 
