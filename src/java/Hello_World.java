abf4a566ecc4b2a88f6983527b55c33859c4c119
lechgarh <lechgar.hatim@ehtp.ac.ma>
2024-09-24 10:49:03 +0200

    add duplicate code

--- src/java/Hello_World.java
+++ src/java/Hello_World.java
@@ -3,4 +3,13 @@ public class Hello_World {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+
+    public void methodA() {
+        System.out.println("This is method A");
+    }
+
+    public void methodB() {
+        // Duplicate code from methodA
+        System.out.println("This is method A");
+    }
 }
